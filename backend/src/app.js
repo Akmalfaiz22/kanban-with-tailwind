92454e65bd4cf29cf383c1587a2c719fa1441025
MohdTanzeel <mohdtanzeel00@gmail.com>
2026-07-09 23:44:07 +0530

    Add API health route

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,12 @@ app.use(cors({
   : [],
     credentials: true
 }))
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Kanban API is running 🚀"
+    });
+});
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16k"}))
 app.use(express.static("public"))
