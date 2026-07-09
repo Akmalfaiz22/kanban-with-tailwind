17597bb379a2776d5f4e4212df59c88331577716
MohdTanzeel <mohdtanzeel00@gmail.com>
2026-07-10 00:15:13 +0530

    Move frontend into backend public

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,29 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN
   ? process.env.CORS_ORIGIN.split(",")
   : [],
     credentials: true
 }))
+
+
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Kanban API is running 🚀"
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16k"}))
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "../public")))
 app.use(cookieParser())
 
 import userRouter from "./routes/user.routers.js";
